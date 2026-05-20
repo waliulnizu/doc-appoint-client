05b9f075713224721fb4b058e60fe4544eca8559
waliulnizu <waliul.nizu@gmail.com>
2026-05-20 19:19:59 +0600

    fix homepage doctor navigation and HeroUI v3 card imports
    Use DoctorCard links on home, resolve Mongo _id for URLs, replace
    CardBody with Card.Content, and read API base URL from env.

---
+++ src/lib/auth-client.js
@@ -0,0 +1,8 @@
+import { createAuthClient } from "better-auth/react";
+
+export const authClient =
+  createAuthClient({
+    baseURL:
+      process.env
+        .NEXT_PUBLIC_SERVER_URL,
+  });
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+const serverUrl =
+  process.env.NEXT_PUBLIC_SERVER_URL ||
+  "http://localhost:5000";
+
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: `${serverUrl}/api`,
 });
 
 export default api;
